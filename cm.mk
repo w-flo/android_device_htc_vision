e017e822c3e80b1d76425868ee5c20d293defd27
Nicholas Flintham <nick@flinny.org>
2012-02-27 23:09:48 +0000

    update BUILD_FINGERPRINT

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_ID=IML74K BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:2.3.3/GRI40/87995:user/release-keys PRIVATE_BUILD_DESC="2.50.405.2 CL87995 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_ID=IML74K BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.0.3/IML74K/87995:user/release-keys PRIVATE_BUILD_DESC="4.0.3 Andromadus release-keys"
 
 PRODUCT_NAME := cm_vision
 PRODUCT_DEVICE := vision
