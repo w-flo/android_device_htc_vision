9e4d60eed90f83bd240a46d6daaf73a54905045d
Jon Kagan <darkspadez@gmail.com>
2012-03-29 17:09:45 -0300

    Update for 4.0.4

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_ID=IML74K BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.0.3/IML74K/87995:user/release-keys PRIVATE_BUILD_DESC="4.0.3 Andromadus release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_ID=IMM76D BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.0.4/IMM76D/87995:user/release-keys PRIVATE_BUILD_DESC="4.0.4 Andromadus release-keys"
 
 PRODUCT_NAME := cm_vision
 PRODUCT_DEVICE := vision
