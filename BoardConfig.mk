680a25192b7d778f380a2a5bcff6cfcb4bfa1d10
Nicholas Flintham <nick@flinny.org>
2012-04-06 22:26:30 +0100

    Vision: Support Android Encryption

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,7 +70,7 @@ BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 
-# Definition required for Legacy USB storage
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
 
 # Use Legacy Trackpad
