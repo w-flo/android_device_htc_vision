f87a3fcab80232ca0e0d2af922a790f5e3f84674
Nicholas Flintham <nick@flinny.org>
2012-06-10 10:29:42 +0100

    update BUILD_ID to IMM76L

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_ID=IMM76I BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.0.4/IMM76I/87995:user/release-keys PRIVATE_BUILD_DESC="4.0.4 Andromadus release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_ID=IMM76L BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.0.4/IMM76L/87995:user/release-keys PRIVATE_BUILD_DESC="4.0.4 Andromadus release-keys"
 
 PRODUCT_NAME := cm_vision
 PRODUCT_DEVICE := vision
