6c3f6f96908543c9ff0e9293ca14cb509fe96ae4
Nicholas Flintham <nick@flinny.org>
2012-08-15 22:17:47 +0100

    build kernel with linaro 4.7
    Change-Id: I2fc587963b72c5c89adb9a532e1a238dbb9321be

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,7 @@ BOARD_FLASH_BLOCK_SIZE := 262144
 # Define kernel config for inline building
 TARGET_KERNEL_CONFIG := vision_andromadus_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/msm7x30-3.0
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-linaro-4.7
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
 TARGET_PREBUILT_KERNEL := device/htc/msm7x30-common/msm7230/kernel
