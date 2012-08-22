1dcc739f84b579c48e28423adf2be90e8a7dac5f
Nicholas Flintham <nick@flinny.org>
2012-08-22 19:20:12 +0100

    Set CM  Bootanim
    Change-Id: Ife02f696633be22426dffae93229c147c718dbce

--- cm.mk
+++ cm.mk
@@ -10,5 +10,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.1.1/JRO03C/87995:user/release-keys PRIVATE_BUILD_DESC="4.1.1 Andromadus release-keys"
 
+TARGET_BOOTANIMATION_NAME := vertical-480
+
 PRODUCT_NAME := cm_vision
 PRODUCT_DEVICE := vision
