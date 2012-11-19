c407853f5195fea9cdaa66a973eb22fc0817b003
Nicholas Flintham <nick@flinny.org>
2012-11-19 16:09:15 +0000

    Bt changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,4 +84,4 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storag
 
 # Use Legacy Trackpad
 BOARD_USE_LEGACY_TRACKPAD := true
-
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/vision/bluetooth
